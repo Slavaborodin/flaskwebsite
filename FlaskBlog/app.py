adbf06788f20ef97a7dc8be47396d44207943151
Slavaborodin <Viacheslav.borodin@ucdconnect.ie>
2021-08-19 20:55:33 +0100

    added user login functionality through authentication

--- FlaskBlog/app.py
+++ FlaskBlog/app.py
@@ -2,10 +2,14 @@ import os
 
 
 from flask import Flask,request,render_template,Blueprint,redirect
-from os import path 
+from os import path
+
+from flask.globals import session 
 from flask_login import LoginManager
 from flaskext.mysql import MySQL
 from flask.helpers import url_for
+
+
 #import the Blueprint from the views file  
 #from .views import views
 #from .authentication import authentication
@@ -49,10 +53,34 @@ app = Flask(__name__)
 # shows the home view
 @app.route("/login",methods=['GET','POST'])
 def login():
-    email=request.form.get("email")
-    password=request.form.get("password")
-    
-    return render_template("login.htm")
+
+    #error message to tell user when something goes wrong 
+    error_msg=''
+
+
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        #get user credentials     
+        email=request.form.get("email")
+        password=request.form.get("password")
+
+        conn=mysql.connect()
+        cursor=conn.cursor()
+
+        cursor.execute('SELECT * FROM User_Logins where user_username =%s and user_password =%s', (email,password,))
+
+        account_status = cursor.fetchone()
+
+        if account_status:
+            session['loggedin'] = True
+            session['id']= account_status['user_id']
+            session['username'] = account_status['user_username']
+            return 'Logged in Successfully'
+        else:
+            error_msg='Incorrect username/password'
+            print(error_msg)
+
+
+    return render_template("login.htm",msg=error_msg)
 
 @app.route("/Sign-up", methods=['GET','POST'])
 def signup():
